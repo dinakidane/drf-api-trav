9b68fdbcb1b259e34c440ba2615dbd295ce27880
dinakidane <dinakidane@hotmail.com>
2024-07-31 13:40:06 +0000

    add admin for notifications

--- notifications/admin.py
+++ notifications/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Notification
 
-# Register your models here.
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ('recipient', 'actor', 'verb', 'created_at', 'read')
+    list_filter = ('read', 'created_at')
+    search_fields = ('recipient__username', 'actor__username', 'verb')
+
