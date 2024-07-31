709c68e628d91eb83621f50c46cbeb8468370268
dinakidane <dinakidane@hotmail.com>
2024-07-31 13:39:02 +0000

    add views for notifications

--- notifications/views.py
+++ notifications/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Notification
+from .serializers import NotificationSerializer
+from drf_api.permissions import IsOwnerOrReadOnly
 
-# Create your views here.
+class NotificationList(generics.ListAPIView):
+    """
+    List notifications for the logged-in user.
+    """
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = NotificationSerializer
+
+    def get_queryset(self):
+        return Notification.objects.filter(recipient=self.request.user)
+
+class NotificationDetail(generics.RetrieveUpdateAPIView):
+    """
+    Retrieve or update a notification (e.g., mark as read).
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Notification.objects.all()
+    serializer_class = NotificationSerializer
