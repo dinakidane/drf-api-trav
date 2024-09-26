93d4739fb6b877c885c45c33f705acee01c1c096
Dina Kidane <dinakidane@hotmail.com>
2024-09-26 04:30:48 +0000

    delete notifications

--- notifications/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Notification
-
-@admin.register(Notification)
-class NotificationAdmin(admin.ModelAdmin):
-    list_display = ('recipient', 'actor', 'verb', 'created_at', 'read')
-    list_filter = ('read', 'created_at')
-    search_fields = ('recipient__username', 'actor__username', 'verb')
-
--- notifications/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('notifications/', views.NotificationList.as_view()),
-    path('notifications/<int:pk>/', views.NotificationDetail.as_view()),
-]
