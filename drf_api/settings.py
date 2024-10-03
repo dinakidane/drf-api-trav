f244566fb6e441b3a66889059ed56d409a8d20e2
Dina Kidane <dinakidane@hotmail.com>
2024-10-03 13:57:22 +0000

    edit the settings for cors origin

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -124,10 +124,10 @@ if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
         os.environ.get('CLIENT_ORIGIN')
     ]
-else:
-    CORS_ALLOWED_ORIGIN_REGEXES = [
-        r"^https://.*\.codeinstitute-ide\.net$",
-    ]
+    
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    r"^https://.*\.codeinstitute-ide\.net$",
+]
 
 CORS_ALLOW_CREDENTIALS = True
 
