48cc04a45a6c2433b5f6021ea5e57d9b29908f89
Dina Kidane <dinakidane@hotmail.com>
2024-10-03 13:14:22 +0000

    Add views for reviews

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Review
+from .serializers import ReviewSerializer
 
-# Create your views here.
+class ReviewListCreate(generics.ListCreateAPIView):
+    """
+    View to list all reviews or create a new review if logged in.
+    """
+    permission_classes = [permissions.IsAuthenticated]  # Only authenticated users can create reviews
+    serializer_class = ReviewSerializer  # Specify the serializer to use
+    queryset = Review.objects.all()  # Get all reviews
+
+    def perform_create(self, serializer):
+        # Associate the current logged in user with the review being created
+        serializer.save(owner=self.request.user)
