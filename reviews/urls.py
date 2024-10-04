c7932e0a507779598cc33aab21c6057074195ead
Dina Kidane <dinakidane@hotmail.com>
2024-10-04 00:15:08 +0000

    add reviews detail to reviews urls

--- reviews/urls.py
+++ reviews/urls.py
@@ -4,4 +4,5 @@ from .views import ReviewListCreate
 urlpatterns = [
     # Endpoint to list all reviews or create a new one
     path('reviews/', ReviewListCreate.as_view(), name='review-list-create'),
+    path('reviews/<int:pk>/', ReviewDetail.as_view(), name='review-detail'), 
 ]
