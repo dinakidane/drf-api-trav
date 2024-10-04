7377d4c2ee8870d02e06363237c9a082447b495e
Dina Kidane <dinakidane@hotmail.com>
2024-10-04 02:24:11 +0000

    add index html to templates folder in src

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -19,6 +19,7 @@ from django.views.generic import TemplateView
 from .views import logout_route
 
 urlpatterns = [
+
     path('', TemplateView.as_view(template_name='index.html')),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
